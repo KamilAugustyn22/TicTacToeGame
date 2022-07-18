141047d5f7a72533c98b511cd7a6bb0f345126e7
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-07-18 14:49:39 +0200

    append inner text

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 let gameboard = document.querySelectorAll(".place");
+let index = 0;
 
 for(let place of gameboard){
     place.addEventListener("click", function (event) {
         console.log("X" + place.id);
+        if(index % 2 == 0){
+            place.innerText = "X";
+            index++;
+        }
+        else{
+            place.innerText = "O"
+            index++;
+        }
     })
 }
